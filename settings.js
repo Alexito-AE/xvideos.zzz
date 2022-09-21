da59cfd0289e21187d60ddd81f0580cef3ff834a
Alexito <109402307+Alexito-AE@users.noreply.github.com>
2022-09-20 21:25:55 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,29 +21,29 @@ global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+global.language = spanyol  //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.botname = "Alexito-Bot" //namabot kalian
+global.ownername= "Leyenda" //nama kalian
+global.myweb ="https://xnxx.com" //bebas asal jan hapus
+global.youtube = "https://youtube.com/xvideos" //bebas asal jan hapus
+global.myweb2 = "https://xvideos.com" //bebas
+global.email = "support@xnxx.com" //bebas
+global.region = "Perú" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['51968374620'] //premium user
+global.owner = ["51968374620"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "51968374620" // nomor wa kalian
+global.ownernomerr = "+51968374620" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'alexito tu papi' //sticker wm ubah
+global.author = 'creador papi Alexito' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
